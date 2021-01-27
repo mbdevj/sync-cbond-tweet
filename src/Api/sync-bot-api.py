dc6fa43617ad4f485443e71f99dc351ceca14b16
Michael B <mbrotmans@gmail.com>
2021-01-26 23:30:40 -0500

    refactoring more

--- src/Api/sync-bot-api.py
+++ src/Api/sync-bot-api.py
@@ -1,33 +1,16 @@
 #!/usr/bin/python3
 
 from flask import Flask, request
-from ..Actions import tweet
+from ..Scenarios import bond_created
 
 app = Flask(__name__)
 
 
 @app.route('/api/v1/tweet-crypto-bond-created', methods=["GET", "POST"])
 def tweet_crypto_bond_created():
-    first_ticker = str(request.args["first-ticker"])
-    first_qty = str(request.args["first-qty"])
-    second_ticker = str(request.args["second-ticker"])
-    second_qty = str(request.args["second-qty"])
-    duration = str(request.args["duration"])
-    apr = str(request.args["apr"])
-    image_path = str(request.args["image-path"])
+    bond_created.tweet()
 
-    tweet_text = "New " + duration + " day $SYNC #CryptoBond created using " \
-                 + first_qty + " $" + first_ticker + " and " + second_qty + " $" + second_ticker \
-                 + ", yielding an APR of " + apr + "%! Create yours now at https://syncbond.com."
 
-    try:
-        tweet.update_status_with_media(tweet_text, image_path)
-        message = "Successfully tweeted."
-    except Exception as e:
-        message = "Failed to tweet."
-        print(e)
-
-    return message
 
 
 app.run(debug=False)
---
+++ src/Scenarios/bond_created.py
@@ -0,0 +1,25 @@
+from flask import request
+from ..Actions import tweet
+
+
+def tweet():
+    first_ticker = str(request.args["first-ticker"])
+    first_qty = str(request.args["first-qty"])
+    second_ticker = str(request.args["second-ticker"])
+    second_qty = str(request.args["second-qty"])
+    duration = str(request.args["duration"])
+    apr = str(request.args["apr"])
+    image_path = str(request.args["image-path"])
+
+    tweet_text = "New " + duration + " day $SYNC #CryptoBond created using " \
+                 + first_qty + " $" + first_ticker + " and " + second_qty + " $" + second_ticker \
+                 + ", yielding an APR of " + apr + "%! Create yours now at https://syncbond.com."
+
+    try:
+        tweet.update_status_with_media(tweet_text, image_path)
+        message = "Successfully tweeted."
+    except Exception as e:
+        message = "Failed to tweet."
+        print(e)
+
+    return message
